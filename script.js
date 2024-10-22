5602043668cbba06b3d8a8776381e0abc39508d7
LukaElizbarashvili <elizbarashvililuka3@gmail.com>
2024-10-22 20:01:40 +0400

    needs some work

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputItem = document.getElementById("Filter");
+const inputResult = document.getElementById("result");
+let listItems = [];
+
+// Get the Api
+async function asyncFnc() {
+    const url = await fetch("https://api.disneyapi.dev/character");
+    try {
+        if (!url.ok) {
+            throw new Error("Can not recieve data");
+        }
+        const mosuliData = await url.json();
+        mosuliData.data.forEach((element) => {
+          const li = document.createElement("li");
+          li.textContent = `${element.name}`;
+          listItems.push(li);
+    
+          inputResult.appendChild(li);
+        });
+      } catch (e) {
+        console.log(e);
+      }
+}
+asyncFnc();
+
+
+// filter
+function filterFnc(searchItem) {
+    listItems.forEach((item) => {
+        if (item.innerText.toLowerCase().includes(searchItem.toLowerCase())) {
+          item.classList.remove("hide");
+        } else {
+          item.classList.add("hide");
+        }
+      });
+}
+inputItem.addEventListener("keyup", function () {
+    filterFnc(this.value);
+});
+try {
+  
+} catch (error) {
+  
+}
